5fa1b89dff681a653a9bf718c63f174f8d67e79a
ZyanHere <133707817+ZyanHere@users.noreply.github.com>
2024-01-28 20:03:10 +0530

    model, hooks,jwt,bcrypt

---
+++ backend/src/models/video.model.js
@@ -0,0 +1,47 @@
+import mongoose, { Schema } from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2"
+
+
+const videoSchema = new Schema({
+    videoFile: {
+        type: String,
+        required: true,
+    },
+    thumbnail: {
+        type: String,
+        required: true,
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    
+    description: {
+        type: String,
+        required: [true,'password is required']
+    },
+    duration: {
+        type: Number,
+        required: true,
+    },
+    views: {
+        type: Number,
+        default: 0,
+    },
+    isPublished: {
+        type: Boolean,
+        default: true
+    },
+    owner: {
+        type: Schema.Types.ObjectId,
+        re: "User"
+    }
+},
+{
+    timestamps: true
+}
+)
+
+videoSchema.plugin(mongooseAggregatePaginate)
+
+export const Video = mongoose.model("Video", videoSchema)
