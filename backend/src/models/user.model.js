e928c880b0f79be3a20aec6a3e52662afaacb0ab
ZyanHere <133707817+ZyanHere@users.noreply.github.com>
2024-01-29 20:44:17 +0530

    cloudinary configured

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -3,51 +3,52 @@ import jwt from "jsonwebtoken"
 import bcrypt from "bcrypt"
 
 
-const userSchema = new Schema({
-    username: {
-        type: String,
-        required: true,
-        unique: true,
-        lowercase: true,
-        trim: true,
-        index: true
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true,
+            index: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+        },
+        fullname: {
+            type: String,
+            required: true,
+            trim: true,
+            index: true
+        },
+        avatar: {
+            type: String,
+            required: true,
+        },
+        coverImage: {
+            type: String,
+        },
+        watchHistory: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "Video"
+            }
+        ],
+        password: {
+            type: String,
+            required: [true,'password is required']
+        },
+        refreshToken: {
+            type: String,
+        },
     },
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-        lowercase: true,
-    },
-    fullname: {
-        type: String,
-        required: true,
-        trim: true,
-        index: true
-    },
-    avatar: {
-        type: String,
-        required: true,
-    },
-    coverImage: {
-        type: String,
-    },
-    watchHistory: [
-        {
-            type: Schema.Types.ObjectId,
-            ref: "Video"
-        }
-    ],
-    password: {
-        type: String,
-        required: [true,'password is required']
-    },
-    refreshToken: {
-        type: String,
-    },
-},
-{
-    timestamps: true
-}
+    {
+        timestamps: true
+    }
 )
 
 userSchema.pre("save", async function (next){
