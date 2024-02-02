2a9788bcab2f5e8e6c1e4c9675650c4d94bd0410
ZyanHere <133707817+ZyanHere@users.noreply.github.com>
2024-02-02 21:17:22 +0530

    added refresh token endpoint

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import {Router} from "express"
-import { registerUser } from "../controllers/user.controller.js"
+import { refreshAccessToken, registerUser } from "../controllers/user.controller.js"
 import {upload} from "../middlewares/multer.middleware.js"
 
 const router = Router()
@@ -19,6 +19,7 @@ router.route("/register").post(
 
     router.route("/login").post(loginUser)
     //secured routes
-    router.route("/logout").post(verifyJWT,  logoutUser)        
+    router.route("/logout").post(verifyJWT,  logoutUser) 
+    router.route("/refresh-token").post(refreshAccessToken)       
 
 export default router
